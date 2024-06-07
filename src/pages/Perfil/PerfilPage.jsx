eb02bf6a4d5990463cc7bcaba59635ad3c41aa9b
Guilherme Cunha <alexandregui013@gmail.com>
2024-06-07 18:38:37 -0300

    tela de Perfil
    Terminei de organizar a tela de perfil, colocando os jogos adquiridos com a avaliação, os botões de alterar e apagar perfil e o de ir para a tela de ADM

--- src/pages/Perfil/PerfilPage.jsx
+++ src/pages/Perfil/PerfilPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import './PerfilPage.css'
-import { HiOutlineMagnifyingGlass } from "react-icons/hi2";
+import { HiOutlineMagnifyingGlass, HiOutlineShoppingCart, HiTrash, HiPencil } from "react-icons/hi2";
 
 const PerfilPage = () => {
+
+  const nome = "<<Nome de Usuário>>";
+
   return (
     <div className='container'>
       <div className='NavBar'>
@@ -20,6 +23,52 @@ const PerfilPage = () => {
             <button className='procurar'><HiOutlineMagnifyingGlass /></button>
           </div>
         </div>
+      </div>
+      <div className='corpo'>
+        <div className='LadoEsquerdo'>
+          <div className='ConteudoEsquerdo'>
+            <h1 className='boasVindas'>Olá, {nome}</h1>
+            <br></br><br></br><br></br>
+            <p className='info'>Seu email é nomedousuario@email.com</p>
+            <br></br>
+            <p className='info'>Seu CPF é 123.456.789-00</p>
+            <br></br><br></br>
+            <button className='botoesPerfil'>
+              <span>Alterar Dados</span> <HiPencil/>
+              </button>
+            <br></br><br></br>
+            <button className='botoesPerfil'>
+              <span>Excluir Conta</span> <HiTrash/>
+              </button>
+            <br></br><br></br>
+            <button className='botoesPerfil' disabled>
+              <span>Tela do admin</span>
+              </button>
+          </div>
+        </div>
+        <div className='LadoDireito'>
+          <div className='ConteudoDireito'>
+            <p className='TituloJogosAd'>Jogos Adquiridos:</p>
+              <div className='jogosAd'>
+                <div className='Jogo1'>
+                  <a className='avaliação'>100/100</a>
+                </div>
+                <div className='Jogo2'>
+                  <a className='avaliação'>95/100</a>
+                </div>
+                <div className='Jogo3'>
+                  <a className='avaliação'>75/100</a>
+                </div>
+                <div className='Jogo4'>
+                  <a className='avaliação'>adicionar avaliação</a>
+                </div>
+              </div>
+
+          </div>
+          <div className='Carrinho'>
+              <HiOutlineShoppingCart/>
+            </div>
+          </div>
       </div>
     </div>
   )
